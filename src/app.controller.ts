f2807435bdd56b2fa6ee9b9d62208009a33f17ad
박완섭 <wanseob.dev@gmail.com>
2024-12-15 20:37:42 +0900

    feat. type orm column

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserModel } from './entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly userRepository: Repository<UserModel>,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('users')
+  getUsers() {
+    return this.userRepository.find();
+  }
+
+  @Post('users')
+  postUser() {
+    return this.userRepository.save({
+      title: 'test title',
+    });
+  }
+
+  @Patch('users/:id')
+  async PatchUser(@Param('id') id: string) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return this.userRepository.save({
+      ...user,
+      title: user.title + '0',
+    });
   }
 }
---
+++ src/entity/user.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  VersionColumn,
+} from 'typeorm';
+
+export enum Role {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Entity()
+export class UserModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    update: false,
+  })
+  title: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.USER,
+  })
+  role: Role;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @VersionColumn()
+  version: number;
+
+  @Column()
+  @Generated('uuid')
+  additionalID: string;
+}
